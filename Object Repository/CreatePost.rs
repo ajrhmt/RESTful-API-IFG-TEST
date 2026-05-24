4ed6bee7d74a478f93452b682d4d4659ef447db2
ajirohimat <ajirohimat03@gmail.com>
2026-05-24 12:41:53 +0700

    fixing

--- Object Repository/CreatePost.rs
+++ Object Repository/CreatePost.rs
@@ -30,7 +30,7 @@
    <migratedVersion>5.4.1</migratedVersion>
    <path></path>
    <restRequestMethod>GET</restRequestMethod>
-   <restUrl>https://fakerestapi.azurewebsites.net/api/v1/</restUrl>
+   <restUrl>https://jsonplaceholder.typicode.com/posts</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
